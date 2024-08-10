a59dbddf898cfa77d853d599065e32957a9bb235
Manos Labrakis <labrakismanos@windowslive.com>
2024-08-10 15:26:43 +0300

    Added cmake helper scripts

---
+++ CMake/helpers.cmake
@@ -0,0 +1,72 @@
+include(ExternalProject)
+include(CMakePackageConfigHelpers)
+
+# Submodule Init Check Function
+function(check_submoduled_initialized MODULED_INITIALIZED_PARENT)
+
+    set(MODULED_INITIALIZED 1)
+
+    if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Modules/NeuralAmpModelerCore/NAM")
+        if(${MODULED_INITIALIZED_PARENT} EQUAL 1)
+            message("Failed to initialize ${CMAKE_CURRENT_SOURCE_DIR}/Modules/NeuralAmpModelerCore\n")
+        endif()
+        set(MODULED_INITIALIZED 0)
+    elseif(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Modules/NeuralAmpModelerCore/Dependencies/eigen/Eigen/src")
+        if(${MODULED_INITIALIZED_PARENT} EQUAL 1)
+            message("Failed to initialize ${CMAKE_CURRENT_SOURCE_DIR}/Modules/NeuralAmpModelerCore/Dependencies/eigen\n")
+        endif()
+            set(MODULED_INITIALIZED 0)
+    endif()
+
+    if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Modules/AudioDSPTools/dsp")
+        if(${MODULED_INITIALIZED_PARENT} EQUAL 1)
+            message("Failed to initialize ${CMAKE_CURRENT_SOURCE_DIR}/Modules/AudioDSPTools\n")
+        endif()
+            set(MODULED_INITIALIZED 0)
+    endif()    
+
+    if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Modules/json/include")
+        if(${MODULED_INITIALIZED_PARENT} EQUAL 1)
+            message("Failed to initialize ${CMAKE_CURRENT_SOURCE_DIR}/Modules/json\n")
+        endif()
+            set(MODULED_INITIALIZED 0)
+    endif()
+
+    if (NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/Modules/ff_meters/ff_meters.h")
+        if(${MODULED_INITIALIZED_PARENT} EQUAL 1)
+            message("Failed to initialize ${CMAKE_CURRENT_SOURCE_DIR}/Modules/ff_meters\n")
+        endif()
+            set(MODULED_INITIALIZED 0)
+    endif()
+
+    if(${MODULED_INITIALIZED_PARENT} EQUAL 0)
+        if (${MODULED_INITIALIZED} EQUAL 1)
+            SET(${MODULED_INITIALIZED_PARENT} ${MODULED_INITIALIZED} PARENT_SCOPE)
+        endif()
+    else()
+        if (${MODULED_INITIALIZED} EQUAL 1)
+            message("\nSubmodules Initialized!\n")
+        endif()
+    endif()
+
+
+endfunction(check_submoduled_initialized)
+
+# Submodule Init Function
+function(init_submodules)
+    set(INIT_SUCCESS 0)
+    check_submoduled_initialized(INIT_SUCCESS)
+
+    if (${INIT_SUCCESS} EQUAL 0) # Check if submodule already initialized
+        message("\nInitializing Git Submodules...\n")
+        find_program(GIT_CMD git REQUIRED)
+        execute_process(COMMAND "${GIT_CMD}" submodule update --init "${CMAKE_CURRENT_SOURCE_DIR}/Modules/AudioDSPTools")
+        execute_process(COMMAND "${GIT_CMD}" submodule update --init --recursive "${CMAKE_CURRENT_SOURCE_DIR}/Modules/NeuralAmpModelerCore")
+        execute_process(COMMAND "${GIT_CMD}" submodule update --init --recursive "${CMAKE_CURRENT_SOURCE_DIR}/Modules/json")
+        execute_process(COMMAND "${GIT_CMD}" submodule update --init --recursive "${CMAKE_CURRENT_SOURCE_DIR}/Modules/ff_meters")
+
+        set(FINAL_CHECK 1)
+        check_submoduled_initialized(FINAL_CHECK)
+
+    endif()
+endfunction(init_submodules)
